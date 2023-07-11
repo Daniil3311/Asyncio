7a8b6107a9133799d384058cfe56a59795e8bf81
Daniil331 <daniilasaev04493@gmail.com>
2023-07-11 21:49:10 +0300

    true hw

--- main.py
+++ main.py
@@ -3,10 +3,9 @@ import datetime
 import aiohttp
 from more_itertools import chunked
 from sqlalchemy import MetaData
+from models import engine, Session, Base, Swapipeople
+#import requests
 
-from models import engine, Session, Base, SwapiPeople
-from sqlalchemy.dialects import postgresql
-from sqlalchemy.dialects.postgresql import insert
 
 metadata = MetaData()
 
@@ -21,24 +20,36 @@ async def get_people(people_id):
 
 async def paste_to_db(persons_json):
     async with Session() as session:
-        #orm_objects = [SwapiPeople(json=item) for item in persons_json]
-
-        session.add_all([
-            SwapiPeople(name='daniil')
-        ])
+        orm_objects = [Swapipeople(
+                                    name=item['name'],
+                                    films=','.join(item['films']),
+                                    eye_color=item['eye_color'],
+                                    birth_year=item['birth_year'],
+                                    species=','.join(item['species']),
+                                    skin_color=item['skin_color'],
+                                    mass=item['mass'],
+                                    homeworld=','.join(item['homeworld']),
+                                    height=item['height'],
+                                    hair_color=item['hair_color'],
+                                    gender=item['gender'],
+                                    starships=','.join(item['starships']),
+                                    vehicles=','.join(item['vehicles']),
+                                    ) for item in persons_json]
+        session.add_all(orm_objects)
         await session.commit()
-
+        #print(persons_json)
 
 async def main():
     async with engine.begin() as con:
         await con.run_sync(Base.metadata.create_all)
 
-    person_coros = (get_people(i) for i in range(1, 2))
-    person_coros_chunked = chunked(person_coros, 5)
-    tasks = []
+    person_coros = (get_people(i) for i in range(20, 70))
+    person_coros_chunked = chunked(person_coros, 10)
+
     for person_coros_chunk in person_coros_chunked:
         persons = await asyncio.gather(*person_coros_chunk)
-        tasks.append(asyncio.create_task(paste_to_db(persons)))
+        asyncio.create_task(paste_to_db(persons))
+    tasks = asyncio.all_tasks() - {asyncio.current_task(), }
     await asyncio.gather(*tasks)
 
 
@@ -47,9 +58,7 @@ if __name__ == "__main__":
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
     asyncio.run(main())
 
-
     print(datetime.datetime.now() - start)
-
 
 
 
--- models.py
+++ models.py
@@ -2,33 +2,31 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm.session import sessionmaker
 from sqlalchemy import JSON, Integer, Column, String, MetaData
-import psycopg2
-from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
 
-# connection = psycopg2.connect(user="postgres")
-# connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-# cursor = connection.cursor()
-#sql_create_database = cursor.execute('create database hw_asyncio')
-# cursor.close()
-# connection.close()
-
-metadata = MetaData()
-PG_DSN = "postgresql+asyncpg://postgres@127.0.0.1:5440/hw1_asyncio"
+PG_DSN = "postgresql+asyncpg://postgres:postgres@127.0.0.1:5440/hw2_asyncio"
 engine = create_async_engine(PG_DSN)
 Base = declarative_base()
 Session = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 print(PG_DSN)
 
 
-class SwapiPeople(Base):
-    __tablename__ = "swapi_people"
-
+class Swapipeople(Base):
+    __tablename__ = 'swapi people'
     id = Column(Integer, primary_key=True)
-    #json = Column(JSON)
-    # # films = Column(JSON)
     name = Column(String)
-
+    films = Column(String)
+    eye_color = Column(String)
+    birth_year = Column(String)
+    species = Column(String)
+    skin_color = Column(String)
+    mass = Column(String)
+    homeworld = Column(String)
+    height = Column(String)
+    hair_color = Column(String)
+    gender = Column(String)
+    starships = Column(String)
+    vehicles = Column(String)
 
 
 
